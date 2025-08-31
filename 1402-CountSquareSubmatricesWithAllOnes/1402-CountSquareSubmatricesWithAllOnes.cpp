aee872e2d88a866935ca67e8aa79bccc310ca4c4
Tanmay Sachin Kashyap <121250816+Tanmay2468@users.noreply.github.com>
2025-08-31 10:49:17 +0530

    Create 1402-CountSquareSubmatricesWithAllOnes

---
+++ 1402-CountSquareSubmatricesWithAllOnes/1402-CountSquareSubmatricesWithAllOnes.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/31/2025, 10:49:21 AM
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        int sum=0;
+        for (int i=1;i<=m;i++)
+        {
+            for (int j=1;j<=n;j++)
+            {
+                if (matrix[i-1][j-1]==1) dp[i][j]=1+min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+                sum+=dp[i][j];
+            }
+        }
+        return sum;
+    }
+};
