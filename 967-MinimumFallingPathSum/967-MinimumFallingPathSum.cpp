02d9106cc8b0c005bb7b6ed5c10a26837dc34a53
Tanmay Sachin Kashyap <121250816+Tanmay2468@users.noreply.github.com>
2025-08-31 10:49:28 +0530

    Create 967-MinimumFallingPathSum

---
+++ 967-MinimumFallingPathSum/967-MinimumFallingPathSum.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/31/2025, 10:49:31 AM
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                if (i==0) dp[i][j]=matrix[i][j];
+                else if (j==0) dp[i][j]=matrix[i][j]+min(dp[i-1][j], dp[i-1][j+1]);
+                else if (j==n-1) dp[i][j]=matrix[i][j]+min(dp[i-1][j], dp[i-1][j-1]);
+                else dp[i][j]=matrix[i][j]+min(dp[i-1][j], min(dp[i-1][j+1], dp[i-1][j-1]));
+            }
+        }
+        return *min_element(dp[n-1].begin(), dp[n-1].end());
+    }
+};
